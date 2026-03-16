e9ba8f4337ad2981ae19c544d91cc4153a735d4c
nyccolas <pradoseixa@gmail.com>
2026-03-16 13:01:03 -0300

    added a min ammount of plants

--- Save0/main.py
+++ Save0/main.py
@@ -2,11 +2,19 @@ from entity_item_log import *
 from unlocks_log import *
 import hay, wood, carrot, pumpkin
 
+min_plant = 25000
+
 clear()
 ei_log(plant_dict)
 un_log(unlock_dict)
 
 while True:
-	pumpkin.farm()
-	
-	
+	while(num_items(Items.Hay) < min_plant):
+		hay.farm()
+	while(num_items(Items.Wood) < min_plant):
+		wood.farm()		
+	while(num_items(Items.Carrot) < min_plant):
+		carrot.farm()
+	while(num_items(Items.Pumpkin) < min_plant):
+		pumpkin.farm()
+	do_a_flip()
